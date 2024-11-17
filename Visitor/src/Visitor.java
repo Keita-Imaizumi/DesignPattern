e662f28ae64979404a31092a70c7ac604b2543df
Keita Imaizumi <Keita.Imaizumi@ibm.com>
2024-11-18 00:38:05 +0900

    feat: Visitor Pattern

--- Visitor/src/Visitor.java
+++ Visitor/src/Visitor.java
@@ -1,4 +1,4 @@
 public abstract class Visitor {
-    public abstract void visit(File file);
-    public abstract void visit(Directory directory);
+	public abstract void visit(File file);
+	public abstract void visit(Directory directory);
 }
